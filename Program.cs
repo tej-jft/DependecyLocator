36f66f8313dc2d3614a8457df9bebd4e05abf661
Tej Prakash <tej.prakash@jellyfishtechnologies.com>
2021-05-07 04:20:04 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DependecyLocator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo d = new DirectoryInfo(@"C:\Program Files (x86)\Default Company Name\Foldio-Desktop-Setup");//Assuming Test is your Folder
+            FileInfo[] Files = d.GetFiles("*.*"); //Getting Text files
+            List<string> str = new List<string>();
+            foreach (FileInfo file in Files)
+            {
+               str.Add(file.Name.ToString());
+            }
+            System.IO.File.WriteAllLines("c:\\Dependency.dat", str);
+        }
+    }
+}
